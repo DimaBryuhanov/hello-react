654d8828736e7bfc3069a1c2d8b505c5cc2e4d67
User <user@Users-MacBook-Pro.local>
2020-07-30 17:25:55 +0300

    Rendering frunctions as React components

--- src/index.js
+++ src/index.js
@@ -3,16 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 //JSX Syntax:
-
-let city = {
-  name: "Madrid",
-  country: "Spain"
-};
+function Hello(){
+  return <h1>Welcome to react</h1>
+}
 
 ReactDOM.render(
-<h1 id='heading' className="cool-text">
-  {city.name} is located in {city.country}
-  </h1>,
+<Hello></Hello>,
   document.getElementById('root')
 );
 
