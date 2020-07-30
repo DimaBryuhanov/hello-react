725cfa78024c629df2d63ae32b0ab94f91e44f27
User <user@Users-MacBook-Pro.local>
2020-07-30 16:58:53 +0300

    Changed the default App to React.createElement

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 ReactDOM.render(
-  React.createElement("h1", null, "Hello!"),
+  React.createElement("div", {style: {color: "blue"}}, React.createElement('h1', null, 'Hi')),
   document.getElementById('root')
 );
 
