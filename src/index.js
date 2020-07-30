d136a3cc99b07063bc1f5b556d45f5f5a3d301aa
User <user@Users-MacBook-Pro.local>
2020-07-30 22:41:44 +0300

    added multiple state variables

--- src/index.js
+++ src/index.js
@@ -6,13 +6,29 @@ import './index.css';
 
 function App() {
   //Hook
+  const [year, setYear] = useState(2050)
   const [status, setStatus] = useState("Open");
+  const [manager, setManager] = useState('John');
   return (
+    <>
+    <div>
+      <h2>Year: {year}</h2>
+      <button onClick={() => setYear(year + 1)}>
+        New Year
+      </button>
+    </div>
+    <div>
+      <h2>Manager on duty: {manager}</h2>
+      <button onClick={() => setManager("Donald")}>
+        New Manager
+      </button>
+    </div>
     <div>
       <h1>Status: {status}</h1>
       <button onClick={() => setStatus("Open")}>Open</button>
       <button onClick={() => setStatus("Closed")}>Closed</button>
     </div>
+  </>
   );
 }
 
