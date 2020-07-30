0831fd92336edce2f2c95aa05ec57ac0eead3696
User <user@Users-MacBook-Pro.local>
2020-07-30 17:40:45 +0300

    added props to the component

--- src/index.js
+++ src/index.js
@@ -3,17 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 //JSX Syntax:
-function Hello(){
+function Hello(props) {
+  console.log(Object.keys(props))
   return (
     <div>
-      <h1>Welcome to react</h1>
-      <p>Let's build something cool!</p>
+      <h1>Welcome to {props.library}</h1>
+      <p>{props.message}</p>
+      <p>React components: {props.number}</p>
     </div>
   )
 }
 
 ReactDOM.render(
-<Hello />,
+  <Hello library="React"
+    message="Have Fun!"
+    number={3}
+  />,
   document.getElementById('root')
 );
 
